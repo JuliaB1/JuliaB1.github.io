1f99c933b570786d4f335610c1df4770bb84c12d
Julia Baylon <juliabaylon01@gmail.com>
2021-07-07 16:44:37 -0700

    styled landing

---
+++ src/pages/Landing.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const center = {
+    position: "fixed",
+    top: "45%",
+    left: "42%",
+    fontSize: 42,
+    fontFamily: "Cormorant Garamond"
+}
+
+const Landing = () => {
+    return (
+        <div style={center}>
+            Julia Baylon
+        </div>
+    )
+}
+
+export default Landing;
