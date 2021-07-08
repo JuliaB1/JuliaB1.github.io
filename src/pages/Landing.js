0d8f6ccf5dc8d1e26670d518bae0d52ad745aad3
Julia Baylon <juliabaylon01@gmail.com>
2021-07-07 18:45:32 -0700

    added filler text

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import About from "./components/About";
 import img from "../images/sfsunset.jpg";
 import downarrow from "../images/downarrow.svg";
-import { Link } from "gatsby";
 
 const pic = {
     margin: "auto",
@@ -19,6 +18,7 @@ const pic = {
     objectFit: "cover"
 };
 
+
 const name = {
     paddingTop: 20,
     fontWeight: "bold",
@@ -33,25 +33,26 @@ const Landing = () => {
         require("smooth-scroll")('a[href*="#"]')
     }
     return (
-        <div>
-            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", paddingBottom: 500, scrollBehavior: "smooth" }}>
-                <img src={img} style={pic} />
-                <div style={name}>
-                    Julia Baylon
-                </div>
-                <div>
-                    <div style={{ fontSize: 18, paddingTop: 70, fontFamily: "Cormorant Garamond", maxWidth: 700, margin: "auto", textAlign: "center" }}>
-                        Hi! I recently graduated from UCLA with a Bachelor of Science in Computer Science and a minor in Linguistics.
-                    </div>
-                </div>
-
-                <a href="#about" style={{ width: 30, margin: "auto", paddingRight: 50, paddingTop: 100, paddingBottom: 100 }}><img src={downarrow} /></a>
-                <div id="about">
-                    <About />
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", paddingBottom: 500, scrollBehavior: "smooth" }}>
+            <img src={img} style={pic} />
+            <div style={name}>
+                Julia Baylon
+            </div>
+            <div>
+                <div style={{ fontSize: 18, paddingTop: 70, fontFamily: "Cormorant Garamond", maxWidth: 700, margin: "auto", textAlign: "center" }}>
+                    Hi! I recently graduated from UCLA with a Bachelor of Science in Computer Science and a minor in Linguistics.
                 </div>
             </div>
 
+            <a href="#about" style={{ width: 30, margin: "auto", paddingRight: 50, paddingTop: 100, paddingBottom: 100 }}><img src={downarrow} /></a>
+            <div id="about" style={{ margin: "auto", minWidth: "80%" }}>
+                <About />
+            </div>
+            <div style={{ fontFamily: "Cormorant Garamond", margin: "auto", paddingTop: 100 }}>
+                © 2021, Julia Baylon.
+            </div>
         </div>
+
 
     )
 }
--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -1,10 +1,32 @@
 import * as React from "react";
 
+const blackBg = {
+    backgroundColor: "black",
+    display: "block",
+    color: "white",
+    fontFamily: "Cormorant Garamond",
+
+    marginLeft: 0,
+    marginRight: 0,
+
+    borderRadius: 5
+}
+
 const About = () => {
     return (
-        <div style={{ backgroundColor: "black", color: "white", width: "100vw", fontFamily: "Cormorant Garamond" }}>
-            <div style={{ fontSize: 36 }}>
+        <div style={blackBg}>
+            <div style={{ fontSize: 36, paddingTop: 50, paddingLeft: 50 }}>
                 About Me
+            </div>
+            <hr style={{ width: "90%" }} />
+            <div style={{ paddingBottom: 50, paddingLeft: 50 }}>
+                <ul>
+                    <li>hi</li>
+                    <li>hi</li>
+                    <li>hi</li>
+                    <li>hi</li>
+                    <li>hi</li>
+                </ul>
             </div>
         </div>);
 }
