c40f2c655545fd17afc2fbf7a24d373d1fafee2a
Julia Baylon <juliabaylon01@gmail.com>
2021-07-07 18:14:23 -0700

    added smooth scroll to (empty) about section

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "My Portfolio",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud", "gatsby-plugin-scroll-reveal"],
 };
---
+++ src/pages/components/About.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const About = () => {
+    return (
+        <div style={{ backgroundColor: "black", color: "white", width: "100vw", fontFamily: "Cormorant Garamond" }}>
+            <div style={{ fontSize: 36 }}>
+                About Me
+            </div>
+        </div>);
+}
+
+export default About;
