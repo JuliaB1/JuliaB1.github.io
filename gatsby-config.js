a7c2d075bd33aaf6c7f9b6b1858193a4f9ad49f4
Julia Baylon <juliabaylon01@gmail.com>
2021-07-09 12:52:41 -0700

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: "My Portfolio",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud", "gatsby-plugin-scroll-reveal", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-scroll-reveal",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },],
 };
