eea2b5f258c3eb25443f5fa7f738835a4a945848
Julia Baylon <juliabaylon01@gmail.com>
2021-07-12 10:40:21 -0700

    added more to about

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -19,10 +19,10 @@ const About = () => {
                 About Me
             </div>
             <hr style={{ width: "90%" }} />
-            <div style={{ paddingBottom: 50, paddingLeft: 50, fontSize: 22, paddingRight: 20 }}>
+            <div style={{ paddingBottom: 50, paddingLeft: 50, fontSize: 22, paddingRight: 50 }}>
                 <ul>
                     <li>I'll be starting work as a software engineer at Facebook in August 2021.</li>
-                    <li>I love to cook, hike, crochet, listen to music, write, and watch aesthetic cooking videos on YouTube.</li>
+                    <li>I love to cook, hike, crochet, listen to music, write, play the occasional video game, and watch aesthetic cooking videos on YouTube.</li>
                     <li>I've been a Nintendo nerd since age 6 when I first played Pokémon Sapphire.</li>
                     <li>Milk is my favorite beverage.</li>
                 </ul>
